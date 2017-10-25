68719d9b0b82bdc982de6efb6671a847c2b21a78
Jose Rosa <jose770@gmail.com>
2017-10-25 00:03:26 -0400

    Initial Commit

--- bin/run_vyos-play.bash
+++ bin/run_vyos-play.bash
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ansible_vyos_home="/usr/local/share/ansible/vyos"
-#/usr/local/bin/ansible-playbook -vvvvv $ansible_vyos_home/vyos-cli-play.yml --vault-password-file $ansible_vyos_home/auth/vault/vault_pass.py
-/usr/bin/ansible-playbook -vvvv $ansible_vyos_home/vyos-cli-play.yml
-#/bin/rm $ansible_vyos_home/*.retry
+# If ansible Vault is used: 
+# /usr/local/bin/ansible-playbook -vvvvv $ansible_vyos_home/vyos-cli-play.yml --vault-password-file $ansible_vyos_home/auth/vault/vault_pass.py
+#
+# For full debug use -d. For normal run use -n
+if [ $1 = "-d" ]
+then
+    /usr/local/bin/ansible-playbook -vvvv $ansible_vyos_home/vyos-cli-play.yml
+elif [ $1 = "-n" ]
+then
+   /usr/local/bin/ansible-playbook $ansible_vyos_home/vyos-cli-play.yml
+else                                
+        echo " Options are -d or -n"
+fi                                  
